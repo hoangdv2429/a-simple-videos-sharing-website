c9017a2ec1304b7933802fbf461365d4554a85cb
Đỗ Việt Hoàng <hoangdv2429@gmail.com>
2021-12-11 18:32:29 +0700

    make changes to errors's code

--- app/models/video.rb
+++ app/models/video.rb
@@ -11,17 +11,17 @@ class Video < ApplicationRecord
 
     def correct_image_type
         if (thumbnail.attached? && !thumbnail.content_type.in?(%w(image/jpeg image/png)))
-            errors.add(:thumbnail, 'thumbnail must be a JPEG or PNG.')
+            errors.add(:thumbnail, 'must be a JPEG or PNG.')
         elsif thumbnail.attached? ==false
-            errors.add(:thumbnail, 'must have an thumbnail attached.')
+            errors.add(:thumbnail, 'must be attached.')
         end
     end
 
     def correct_video_type
         if(clip.attached? && !clip.content_type.in?(%w(clip/mp4)))
-            errors.add(:clip, 'Video must be a MP4.')
+            errors.add(:clip, 'must be MP4.')
             elsif clip.attached? ==false
-            errors.add(:clip, 'must have an video attached.')
+            errors.add(:clip, 'must be attached.')
         end
     end
 end
