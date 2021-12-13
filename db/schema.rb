d2b855706142a52eded3fd53e68668dcee3ed97b
Minh Trong <minhtrong2k@gmail.com>
2021-12-13 21:31:47 +0700

    Add user name and time comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_13_085722) do
+ActiveRecord::Schema.define(version: 2021_12_13_142231) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2021_12_13_085722) do
     t.string "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "time"
   end
 
   create_table "impressions", force: :cascade do |t|
